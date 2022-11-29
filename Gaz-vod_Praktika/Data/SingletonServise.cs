48dfcde0abb03261b56957a1eb6c5bc4d99c42b7
zubr radel <gggg26705@gmail.com>
2022-11-29 22:14:57 +0300

    Добавьте файлы проекта.

---
+++ Gaz-vod_Praktika/Data/SingletonServise.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace Gaz_vod_Praktika.Data
+{
+    public class SingletonServise
+    {
+        User user;
+
+        public User GetUser()
+        {
+
+            return user;
+        }
+        public void SetUser(User newUser)
+        {
+            user = newUser;
+        }
+    }
+
+}
